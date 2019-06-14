d450f6a33f1808fd83f52e3d480932ceb014b71f
NambiarSidharth <32272949+NambiarSidharth@users.noreply.github.com>
2019-06-14 22:20:33 +0530

    added card layout

--- Components/Albums/AlbumDetail.js
+++ Components/Albums/AlbumDetail.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-
+import Card from "../Card/Card"
 export default class AlbumDetail extends Component {
     render() {
         const {details} = this.props
         return (
             <View>
-                <Text> {details.title} </Text>
+                <Card detail={details} />
             </View>
         )
     }
---
+++ Components/Card/Card.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { Text, View , StyleSheet} from 'react-native'
+
+export default class Card extends Component {
+    render() {
+        const {detail} = this.props
+        return (
+            <View style={styles.containerStyle}>
+            <Text>{detail.title}</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    containerStyle:{
+        borderRadius:2,
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+        borderColor:"#000",
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop:10
+    }
+})
